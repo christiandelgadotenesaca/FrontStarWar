8786b3108d839d80c723694359a6762d62effbae
christiandelgadotenesaca <christian4delgado@gmail.com>
2023-10-12 22:21:01 -0500

    mejora de estilos

--- api-rest/scripts.js
+++ api-rest/scripts.js
@@ -4,10 +4,18 @@ function personaje(texto) {
     let div = document.createElement('div')
     let h1_texto = document.createTextNode(texto.name)
     let h1 = document.createElement('h1')
+    let h_2texto = document.createTextNode(texto.height)
+    let h2 = document.createElement('h2')
     h1.appendChild(h1_texto)
+    h1.style.color = "yellow"
+    h1.style.fontSize = "24px"
     div.appendChild(h1)
-
+    h2.appendChild(h_2texto)
+    h2.style.color = "green"
+    h2.style.fontSize = "18px"
+    div.appendChild(h2)
     let contenedor = document.getElementById('contenedor')
+    contenedor.style.backgroundColor = "black"
     contenedor.appendChild( div )
 }
 
